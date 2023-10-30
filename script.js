d979418ac753f9639dfc593b4740a0a44133c72e
Sergio Sánchez Zavala <28694175+chekos@users.noreply.github.com>
2023-10-29 18:11:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function captureCard() {
     html2canvas(document.getElementById("card")).then(function(canvas) {
-        // Convert the canvas to a data URL and open it in a new window
-        window.open(canvas.toDataURL());
+        // Appending the canvas directly to the body
+        document.body.appendChild(canvas);
     });
 }
